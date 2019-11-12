56258245d2604274b190d8f1482044a3c19d94ac
lordzth666 <46035970+lordzth666@users.noreply.github.com>
2019-11-12 17:03:31 -0500

    Add files via upload

---
+++ batch_loader.py
@@ -0,0 +1,94 @@
+import numpy as np
+from nltk.corpus import brown
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm 
+
+def create_onehot_vector(idx, num_classes=31):
+    one_hot_vector = np.zeros(num_classes)
+    one_hot_vector[idx] = 1
+    return one_hot_vector
+
+def tokenize(rstr, window_size=35):
+    # 0: <START> 1-27: [a-z] 28: ' ' 29: <EOS> 30: <NULL>
+    tokens = []
+    tokens.append(create_onehot_vector(0))
+    rstr_lower = rstr.lower()
+    for x in rstr_lower:
+        if 'a' <= x and x <= 'z':
+            tokens.append(create_onehot_vector(ord(x)-96))
+        if x == ' ':
+            tokens.append(create_onehot_vector(28))
+    tokens.append(create_onehot_vector(29))
+    while np.shape(tokens)[0] < window_size:
+        tokens.append(create_onehot_vector(30))
+    tokens = np.asarray(tokens)
+    return tokens
+            
+def generate_batch_data(words, clipping_window_size=35):
+    # generate number of words for a string
+    num_words = np.random.randint(2, 5)
+    rand_idx = np.random.choice(np.arange(np.shape(words)[0]), num_words)
+    words_ = words[rand_idx]
+    # randomly insert 'marco': Data Augmentation
+    p = np.random.rand()
+    if p < 0.5:
+        words_ = np.append(words_, 'marco')
+    elif p < 0.6:
+        words_ = np.append(words_, 'mar')
+        words_ = np.append(words_, 'co')
+    elif p < 0.7:
+        words_ = np.append(words_, 'ma')
+        words_ = np.append(words_, 'rco')
+
+    np.random.shuffle(words_)
+    rstr = ' '.join(words_)
+    rstr = rstr[:clipping_window_size-2]
+    data = tokenize(rstr, clipping_window_size)
+    label = (rstr.find('marco') != -1)
+    return data, label
+
+class BatchLoader:
+    def __init__(self, num_data):
+        self.words = []
+        print("Preprocessing batch loader...")
+        for word in brown.words():
+            if not word in [".", "\t", " ", "?", "!", '*', "\'", "\"", ","]:
+                self.words.append(word.strip('\''))
+        self.words = np.asarray(self.words)
+        print("Preprocessing finished!")
+        data_examples = num_data
+
+        data_X = []
+        data_y = []
+        print("Generating data...")
+        for i in tqdm(range(data_examples)):
+            tokens, label = generate_batch_data(self.words)
+            data_X.append(tokens)
+            data_y.append(label)
+
+        data_X = np.asarray(data_X, dtype=np.float32)
+        data_y = np.asarray(data_y, dtype=np.long)
+
+        print(data_X.shape)
+        print(data_y.shape)
+
+        self.data_train_X, self.data_test_X, self.data_train_y, self.data_test_y = train_test_split(data_X, data_y, test_size=0.20)
+        self.num_train_examples = np.shape(self.data_train_X)[0]
+        self.num_test_examples = np.shape(self.data_test_X)[0]
+
+    def get_train_batch(self, batch_size=32):
+        batch_id = np.random.choice(np.arange(self.num_train_examples), batch_size)
+        return self.data_train_X[batch_id, :], self.data_train_y[batch_id]
+
+    def get_test_batch(self, batch_size=32, id=0):
+        batch_id = np.arange(id*batch_size, id*batch_size+batch_size)
+        return self.data_test_X[batch_id, :], self.data_test_y[batch_id]
+
+
+        
+
+    
+    
+        
+    
+    
---
+++ main.py
@@ -0,0 +1,137 @@
+import torch
+import torch.nn as nn
+from torch import zeros, cat
+import torch.nn.functional as F
+from batch_loader import BatchLoader, tokenize
+import numpy as np
+
+WINDOW_SIZE = 35
+# [a-z] [START] [EOS] [NULL]
+NUM_CLASSES = 31
+
+class RNNEncoder(nn.Module):
+    def __init__(self, units, time_stamp=WINDOW_SIZE):
+        super(RNNEncoder, self).__init__()
+        self.W = nn.Linear(units*2, units)
+
+    def forward(self, x):
+        batch_dim = x.size(0)
+        time_dim = x.size(1)
+        out_dim = x.size(2)
+        hidden_state = zeros([batch_dim, out_dim], dtype=torch.float)
+        for i in range(WINDOW_SIZE):
+            x_in = cat([x[:, i, :], hidden_state], dim=1)
+            out = self.W(x_in)
+            out = F.relu(out, inplace=True)
+            hidden_state = out
+        return out
+
+class OneShotRNNDecoder(nn.Module):
+    def __init__(self, units, num_classes=2):
+        super(OneShotRNNDecoder, self).__init__()
+        self.W = nn.Linear(units, num_classes) 
+
+    def forward(self, x):
+        return self.W(x)
+
+
+class Seq2SeqModel(nn.Module):
+    def __init__(self, units, num_classes=31, window_size=WINDOW_SIZE):
+        super(Seq2SeqModel, self).__init__()
+        self.embedding = nn.Linear(num_classes, units)
+        self.encoder = RNNEncoder(units, window_size)
+        self.decoder = OneShotRNNDecoder(units)
+
+    def forward(self, x):
+        return self.decoder(self.encoder(self.embedding(x)))
+
+def trainval(model, data_loader):
+    EPOCHS = 20
+    train_batch_size = 32
+    test_batch_size = 32
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.RMSprop(model.parameters(), lr=1e-4, eps=1e-8, momentum=0.9, weight_decay=1e-4)
+
+    global_steps = 0
+
+    for i in range(EPOCHS):
+        train_steps = data_loader.num_train_examples // train_batch_size
+        test_steps = data_loader.num_test_examples // test_batch_size
+
+        for j in range(train_steps):
+            inputs, targets = data_loader.get_train_batch(batch_size=train_batch_size)
+            inputs, targets = torch.from_numpy(inputs), torch.from_numpy(targets)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            # Update the weights
+            loss.backward()
+            optimizer.step()
+            # Increase global step and print out log
+            global_steps += 1
+            if global_steps % 50 == 0:
+                bin_acc = outputs.argmax(1).eq(targets).sum().item()
+                bin_acc = float(bin_acc) / float(outputs.size(0))
+                print("[Step=%d]\tLoss=%f\tAccuracy=%f" %(global_steps, loss.item(), bin_acc))
+            pass
+        
+        avg_loss = 0
+        avg_acc = 0
+
+        print("Validation...")
+
+        with torch.no_grad():
+            for j in range(test_steps):
+                inputs, targets = data_loader.get_test_batch(batch_size=train_batch_size, id=j)
+                inputs, targets = torch.from_numpy(inputs), torch.from_numpy(targets)
+                outputs = model(inputs)
+                loss = criterion(outputs, targets)
+                bin_acc = outputs.argmax(1).eq(targets).sum().item()
+                bin_acc = float(bin_acc) / float(outputs.size(0))
+                avg_acc += bin_acc / test_steps
+                avg_loss += loss / test_steps
+                pass
+        print("Validation Loss=%f\tValidation Accuracy=%f" %(avg_loss, avg_acc))
+    pass
+
+def inference(model, inputs, targets):
+    with torch.no_grad():
+        tokenized_inputs = tokenize(inputs, window_size=WINDOW_SIZE)
+        tokenized_inputs = np.asarray(tokenized_inputs, dtype=np.float32)
+        tokenized_inputs = np.expand_dims(tokenized_inputs, axis=0)
+        tokenized_inputs = torch.from_numpy(tokenized_inputs)
+
+        outputs = model(tokenized_inputs).cpu().numpy()
+
+    print("Target is: [%s]" %targets)
+
+    if outputs[0][0] < outputs[0][1]:
+        print("Actual output is: [polo ]")
+    else:
+        print("Actual output is: [ ]")
+    pass
+
+
+def main():
+    np.random.seed(233)
+    data_loader = BatchLoader(10000)
+    model = Seq2SeqModel(10, NUM_CLASSES, WINDOW_SIZE)
+    trainval(model, data_loader)
+
+    # Should print 'polo '
+    inference(model, "nor macro I", "polo ")
+    inference(model, "macro nor I", "polo ")
+    inference(model, "nor I macro", "polo ")
+    
+    # Should print ' '
+    inference(model, "nor I neither", " ")
+
+    # More difficult task
+    inference(model, "nor ma rco I", " ")
+    inference(model, "ma rco nor I", " ")
+
+    pass
+
+if __name__ == "__main__":
+    main()
